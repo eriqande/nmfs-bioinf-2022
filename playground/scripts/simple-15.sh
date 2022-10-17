3ec730635ec04171b02872e59d05761896f17804
Eric C. Anderson <eric.anderson@noaa.gov>
2022-10-17 06:47:04 -0600

    more simple 15

--- playground/scripts/simple-15.sh
+++ playground/scripts/simple-15.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 
+(
+  echo "(stdout) Running with Slurm Job ID: $SLURM_JOB_ID" 
+  echo "(stdout) Note that we are running this thing through"
+  echo "(stdout) the tee utility to try to unbuffer it."
+  echo
+) | tee simple-15.stdout
 
-stdbuf -o0 echo "(stdout) Running with Slurm Job ID: $SLURM_JOB_ID"
-stdbuf -o0 echo "(stdout) Note that the stdbuf -o0 is used here because"
-stdbuf -o0 echo "(stdout) without it, the stdout gets stored in a buffer"
-stdbuf -o0 echo "(stdout) and doesn't actually get written to the stream."
-stdbuf -o0 echo
 echo "(stderr) This line is being written to stderr" > /dev/stderr
-echo "(stderr) Interestingly, stderr does not seem to get buffered." > /dev/stderr
+echo "(stderr) Interestingly, stderr does not seem to get buffered." >> /dev/stderr
 
 
 sleep 900
