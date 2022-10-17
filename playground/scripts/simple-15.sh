711ede2646be5dbf5e95418a17d774a17d4b50f3
Eric C. Anderson <eric.anderson@noaa.gov>
2022-10-17 06:31:46 -0600

    update simple-15

--- playground/scripts/simple-15.sh
+++ playground/scripts/simple-15.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 
-echo "Running with Slurm Job ID: $SLURM_JOB_ID"
 
-echo "This line is being written to stderr" > /dev/stderr
+stdbuf -o0 echo "(stdout) Running with Slurm Job ID: $SLURM_JOB_ID"
+stdbuf -o0 echo "(stdout) Note that the stdbuf -o0 is used here because"
+stdbuf -o0 echo "(stdout) without it, the stdout gets stored in a buffer"
+stdbuf -o0 echo "(stdout) and doesn't actually get written to the stream."
+stdbuf -o0 echo
+echo "(stderr) This line is being written to stderr" > /dev/stderr
+echo "(stderr) Interestingly, stderr does not seem to get buffered." > /dev/stderr
+
 
 sleep 900
 
